6549bbbc536a7e1effadfdcfb8d03e6a665b7835
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2020-07-01 14:59:05 +0900

    added ci

---
+++ .github/travis.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+
+apt-get update && apt-get install -y \
+ 			  libflann-dev \
+			  liboctomap-dev \
+ 			  libpcl-dev
+
+if [ -z ${REPO_NAME} ];
+then
+    export REPO_NAME=octomap_server2
+fi
+
+if [ -z ${TRAVIS_BUILD_DIR} ];
+then
+    export TRAVIS_BUILD_DIR=/home/src/$REPO_NAME
+fi
+
+export WORKDIR=${HOME}/ros2/${ROS_DISTRO}/src
+
+if [ ! -d $WORKDIR ];
+then
+    mkdir -p $WORKDIR 
+fi
+
+cd $WORKDIR/..
+mkdir -p src/$REPO_NAME
+cp -r ${TRAVIS_BUILD_DIR} src/
+
+vcs import ./src/$REPO_NAME < src/${REPO_NAME}/deps.repos
+colcon build --symlink-install
