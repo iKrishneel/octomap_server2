641a81b51dc4b30f8e50f6a7e693cd8087fb24d9
Jari Nippula <jarix@ssrc.tii.ae>
2022-02-02 11:24:42 +0200

    entrypoint.sh: Check SIMULATION var for use_sim_time flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 source /opt/ros/galactic/setup.bash
-ros2 launch octomap_server2 octomap_server.py
+
+ROS_FLAGS=""
+if [ ${SIMULATION+x} != "" ]; then
+    ROS_FLAGS="use_sim_time:=true ${ROS_FLAGS}"
+fi
+
+ros2 launch octomap_server2 octomap_server.py ${ROS_FLAGS}
