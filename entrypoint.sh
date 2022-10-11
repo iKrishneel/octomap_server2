d36ac54115aa410e2d87eef533722472cd42c53c
Mehmet Killioglu <mehmet.killioglu@unikie.com>
2022-10-11 12:06:36 +0300

    Update entrypoint to return 125 if code 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,12 @@ if [[ ${SIMULATION+x} != "" ]]; then
 fi
 
 exec ros-with-env ros2 launch octomap_server2 octomap_server.py ${ROS_FLAGS}
+
+RESULT=$?
+if [ $RESULT -ne 0 ]; then
+		echo "ERROR: Octomap server failed with code $RESULT" >&2
+		exit $RESULT
+else
+		echo "INFO: Octomap server finished successfully, but returning 125 code for docker to restart properly." >&2
+		exit 125
+fi
