a45f2468069ccf2370f8c6ce02cd7e042bf34f15
Timo Sairiala <timo.sairiala@unikie.com>
2023-02-04 21:29:29 +0200

    run coverity on self-hosted runner

---
+++ entrypoint-coverity.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+cd /main_ws/src/
+/cov/bin/cov-manage-im --mode auth-key --host --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
+/cov/bin/coverity scan
+/cov/bin/coverity list
