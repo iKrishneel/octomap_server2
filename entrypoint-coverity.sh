b5e78df3c3ab5c3539942ffe859fc9b354908223
Timo Sairiala <timo.sairiala@unikie.com>
2023-02-09 15:17:26 +0200

    exclude java

--- entrypoint-coverity.sh
+++ entrypoint-coverity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 /cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
-/cov/bin/coverity scan
+/cov/bin/coverity scan --exclude-language java
 /cov/bin/coverity list
