a14848998e7aa4f129bd2ce08c7ee16c3b83b44f
Timo Sairiala <timo.sairiala@unikie.com>
2023-02-09 16:45:45 +0200

    add echo prints

--- entrypoint-coverity.sh
+++ entrypoint-coverity.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "running cov manage im"
 /cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
+echo "running coverity scan exclude java"
 /cov/bin/coverity scan --exclude-language java
+echo "running coverity list"
 /cov/bin/coverity list
