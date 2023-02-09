709613f9b77d5d228983671da147f2bc230c9b17
Timo Sairiala <timo.sairiala@unikie.com>
2023-02-09 17:01:11 +0200

    run license fetch in dockerfile and scanning in entrypoint

--- entrypoint-coverity.sh
+++ entrypoint-coverity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "running cov manage im"
-/cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
+echo "running cov manage im - ran in dockerfile"
+#/cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
 echo "running coverity scan exclude java"
 /cov/bin/coverity scan --exclude-language java
 echo "running coverity list"
