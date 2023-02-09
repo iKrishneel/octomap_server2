5ea36bddc82976bd298660f1a80a06957d64c8d1
Timo Sairiala <timo.sairiala@unikie.com>
2023-02-09 15:14:17 +0200

    change coverity parameters

--- entrypoint-coverity.sh
+++ entrypoint-coverity.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd /main_ws/src/
-ls -al
-ls -al /
-mount
-ls -al /cov
-/cov/bin/cov-manage-im --mode auth-key --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
+/cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
 /cov/bin/coverity scan
 /cov/bin/coverity list
