ff100c1d5c551a54f9e5faebfebd58001ad040e6
Timo Sairiala <timo.sairiala@unikie.com>
2023-02-25 10:49:25 +0200

    use fogrosbaseimage_coverity for scanning

--- entrypoint-coverity.sh
+++ entrypoint-coverity.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/bash -eu
 
-echo "running cov manage im - ran in dockerfile"
-#/cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
-echo "running coverity scan exclude java"
-/cov/bin/coverity scan --exclude-language java
-echo "running coverity list"
+echo "pull coverity token"
+/cov/bin/cov-manage-im --mode auth-key --create --output-file auth_key.txt --url https://coverity.ssrc.fi:443 --user cov_scan --set description:"ci test key" --set expiration:"after_1_days"
+authentication_id=$(grep -ow "id\":[[:digit:]]\+" auth_key.txt |grep -ow '[[:digit:]]\+')
+
+echo "running coverity scan"
+/cov/bin/coverity scan
 /cov/bin/coverity list
+
+echo "revoking authentication token"
+/cov/bin/cov-manage-im --mode auth-key --revoke $authentication_id --url https://coverity.ssrc.fi:443 --user cov_scan
